dbf34fad469692dca571a3a5579830679d4d7bab
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-23 17:34:59 +0100

    création arborescence des fichiers

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livre d'or || Page d'accueil</title>
+    <link href="./Style/styles.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/index.php">Accueil</a></li>
+                <li><a href="/inscription.php">Inscription</a></li>
+                <li><a href="/connexion.php">Connexion</a></li>
+                <li><a href="/profil.php">Profil</a></li>
+                <li><a href="/livre-or.php">Livre d'or</a></li>
+                <li><a href="/commentaire.php">Commentaire</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
