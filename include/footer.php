fd1fdb9133b0f73eab7e7a106523d7c5b3d191cf
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-12-01 14:42:01 +0100

    expore BDD

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
 <footer class="globalFooter">
     <p>Boris Tikhomiroff</p>
-    <a href="https://github.com/boris-tikhomiroff/livre-or" class="globalFooter_githubLogo"><img src="./assets/images/Github_Icon" alt="Github logo"></a>
+    <a href="https://github.com/boris-tikhomiroff/livre-or" class="globalFooter_githubLogo"><img src="./assets/images/Github_Icon" alt="Github logo" target="_blank"></a>
 </footer>
